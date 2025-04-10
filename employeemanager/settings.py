5df4006de763b2514d1d978ab73e45d748235960
mstailor <tailorms@rknec.edu>
2025-04-10 19:55:47 +0530

    update code

--- employeemanager/settings.py
+++ employeemanager/settings.py
@@ -82,12 +82,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'employee_db',
-        'USER': 'root',  # Replace with your MySQL username
-        'PASSWORD': 'Mysql5253',  # Replace with your MySQL password
+        'USER': 'admin',
+        'PASSWORD': 'mohd5253',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
+
 
 
 # Password validation
@@ -128,7 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
