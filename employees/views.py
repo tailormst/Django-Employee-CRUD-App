720d6a9aea21d3f3eb53206e65845627ca4e9f05
mstailor <tailorms@rknec.edu>
2025-04-11 19:03:14 +0530

    updated code

--- employees/views.py
+++ employees/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
+from django.forms import modelform_factory
 from .models import Employee, Department
-from .forms import EmployeeForm, DepartmentForm
 
 # Employee Views
 class EmployeeListView(ListView):
@@ -18,20 +18,20 @@ class EmployeeDetailView(DetailView):
 
 class EmployeeCreateView(CreateView):
     model = Employee
-    form_class = EmployeeForm
+    form_class = modelform_factory(Employee, exclude=[])
     template_name = 'employees/employee_form.html'
     success_url = reverse_lazy('employee-list')
-    
+
     def form_valid(self, form):
         messages.success(self.request, 'Employee created successfully!')
         return super().form_valid(form)
 
 class EmployeeUpdateView(UpdateView):
     model = Employee
-    form_class = EmployeeForm
+    form_class = modelform_factory(Employee, exclude=[])
     template_name = 'employees/employee_form.html'
     success_url = reverse_lazy('employee-list')
-    
+
     def form_valid(self, form):
         messages.success(self.request, 'Employee updated successfully!')
         return super().form_valid(form)
@@ -40,7 +40,7 @@ class EmployeeDeleteView(DeleteView):
     model = Employee
     template_name = 'employees/employee_confirm_delete.html'
     success_url = reverse_lazy('employee-list')
-    
+
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, 'Employee deleted successfully!')
         return super().delete(request, *args, **kwargs)
@@ -53,20 +53,20 @@ class DepartmentListView(ListView):
 
 class DepartmentCreateView(CreateView):
     model = Department
-    form_class = DepartmentForm
+    form_class = modelform_factory(Department, exclude=[])
     template_name = 'employees/department_form.html'
     success_url = reverse_lazy('department-list')
-    
+
     def form_valid(self, form):
         messages.success(self.request, 'Department created successfully!')
         return super().form_valid(form)
 
 class DepartmentUpdateView(UpdateView):
     model = Department
-    form_class = DepartmentForm
+    form_class = modelform_factory(Department, exclude=[])
     template_name = 'employees/department_form.html'
     success_url = reverse_lazy('department-list')
-    
+
     def form_valid(self, form):
         messages.success(self.request, 'Department updated successfully!')
         return super().form_valid(form)
@@ -75,7 +75,7 @@ class DepartmentDeleteView(DeleteView):
     model = Department
     template_name = 'employees/department_confirm_delete.html'
     success_url = reverse_lazy('department-list')
-    
+
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, 'Department deleted successfully!')
         return super().delete(request, *args, **kwargs)
